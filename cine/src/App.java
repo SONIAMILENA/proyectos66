0271d80c7bc9079b7cec31a89e674f2434320d20
SONIAMILENA <soniamilenam@gmail.com>
2021-07-27 11:07:17 +0200

    ctualizado cine completo

--- cine/src/App.java
+++ cine/src/App.java
@@ -1,54 +1,93 @@
-
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bienvenidos al cine");
-        System.out.println( );
+        System.out.println("Bienvenido al cine");
 
+        // Cliente Uno
+
+        Scanner sca = new Scanner(System.in);
+        System.out.println("Ingrese su cedula: ");
+        String cedula = sca.nextLine();
+        System.out.println("Ingrese su nombre: ");
+        String nombre = sca.nextLine();
 
         Cliente clienteUno = new Cliente();
-
-        clienteUno.setCedula("1234567890");
-        clienteUno.setNombre("juan");
-        clienteUno.setid(1);
-        clienteUno.mostrarDatos();
+        clienteUno.setCedula(cedula);
+        clienteUno.setNombre(nombre);
+        clienteUno.setId(1);
         clienteUno.generarSaludo();
-        clienteUno.generarSaludo("Buenos Dias");
-        float costo = clienteUno.calcularCosto(10000);
-                System.out.println("CEDULA: "+ clienteUno.getCedula() + " \nNombre: "
+        clienteUno.generarSaludo("Buenos dias");
+        clienteUno.mostrarDatos();
+        float costo = clienteUno.calcularCosto(8000);
+
+        System.out.println("Cedula: " + clienteUno.getCedula() + "\nNombre: "
                             + clienteUno.getNombre() + "\nID: " + clienteUno.getId()
-                            + "\nCosto Tiquete: " + costo ); 
+                            + "\nCosto tiquete: " + costo);
 
-        System.out.println( );
-
-        System.out.println( );
-        System.out.println("Bienvenidos al cine");
-        System.out.println( );
+        // Cliente Dos
 
         ClientePremium clienteDos = new ClientePremium();
-
-        clienteDos.setCedula("1324567870");
-        clienteDos.setNombre("Maria");
+        clienteDos.setCedula("11657777");
+        clienteDos.setNombre("Jose");
         clienteDos.setNumeroTarjeta(100);
         clienteDos.mostrarDatos();
-        clienteDos.generarSaludo();
-        clienteDos.generarSaludo("Buenos Dias ");
 
-        System.out.println( );
+        System.out.println();
         float costoDos = clienteDos.calcularCosto(8000);
 
-        System.out.println("CEDULA: "+ clienteDos.getCedula() + " \nNombre: "
-                            + clienteDos.getNombre() + "\nNumero Tarjeta: " + clienteDos.getNumeroTarjeta()
-                            + "\nCosto Tiquete: " + costoDos );
-        // prueba de contenedores
+        System.out.println("Cedula: " + clienteDos.getCedula() + "\nNombre: "
+                            + clienteDos.getNombre() + "\nNumero tarjeta: "
+                            + clienteDos.getNumeroTarjeta()
+                            + "\nCosto tiquete: " + costoDos);
+
+
+        // Prueba de contenedores
         System.out.println();
         Prueba pruebaContenedor = new Prueba();
         pruebaContenedor.generarLista();
 
 
+        // Lista de objetos
+        System.out.println("CREACION DE CLIENTES");
+        String opcion = "S";
+        while (opcion.equalsIgnoreCase("S")) { // Ignora mayúsculas
+            // Ingresa datos por teclado
+            System.out.println("Ingrese su cedula:");
+            cedula = sca.next(); // Uso de next
+            sca.nextLine();
+            System.out.println("Ingrese su nombre:");
+            nombre = sca.nextLine();
+            System.out.println("Ingrese su ID:");
+            int id = sca.nextInt();
+
+            // Crea objeto cliente
+            /*Cliente nuevoCliente = new Cliente();
+            nuevoCliente.setCedula(cedula);
+            nuevoCliente.setNombre(nombre);
+            nuevoCliente.setId(id);*/
+            Cliente nuevoCliente = new Cliente(cedula, nombre, id);
+
+            // Agregar cliente a la lista
+            pruebaContenedor.agregarCliente(nuevoCliente);
+
+            // Preguntar nuevo cliente
+            System.out.println("Desea agregar otro cliente? (s / n):");
+            opcion = sca.next();
+            sca.nextLine();
+        }
+
+        pruebaContenedor.mostrarNombreClientes();
+        pruebaContenedor.mostrarDatosClientes();
 
 
-
-
+        /*
+        ** Estructura Try - Catch
+        try {
+            // Algoritmo
+        } catch(Exception e){
+            // Proceso realizado si ocurre un eror
+        }
+        */
     }
 }
--- cine/src/Cliente.java
+++ cine/src/Cliente.java
@@ -1,5 +1,17 @@
-public class Cliente extends Persona implements Tiquete {
+public class Cliente extends Persona implements Tiquete{
+
     private int id;
+
+    public Cliente() {
+
+    }
+
+    // Constructor sobrecargado
+    public Cliente(String cedula, String nombre, int id) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.id = id;
+    }
 
     @Override
     public float calcularCosto(float costoBase) {
@@ -7,18 +19,19 @@ public class Cliente extends Persona implements Tiquete {
         return costoReal;
     }
 
-    public void setid(int id) {
+    public void setId(int id) {
         this.id = id;
     }
-    public int getId() { 
-        return this.id;
 
+    public int getId() {
+        return this.id;
     }
+
     @Override
     public void mostrarDatos() {
-        System.out.println(" Cedula = " +  this.cedula 
-                            + " Nombre = " + this.nombre
-                            + " ID" + this.id);
+        System.out.println("Cedula = " + this.cedula
+                        + " Nombre = " + this.nombre
+                        + " ID = " + this.id);
     }
 
 }
